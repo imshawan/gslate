d5372959c8503f583400ec4127d83e32ee941d3a
Shawan Mandal <shawan.sm049@gmail.com>
2021-11-28 21:04:08 +0530

    Fixed test

--- test/index.test.js
+++ test/index.test.js
@@ -1,21 +1,25 @@
 const assert = require('chai').assert;
+var Should = require('chai').should();
+const { expect } = require('chai');
 const Translater = require('../lib/index');
 
 describe('Testing the Translater module', () => {
     
-    it('It should return a JSON response of all the supported languages', () => {
+    it('It should return a JSON response of all the supported languages', (done) => {
         Translater.getLanguages()
         .then((response) => {
             assert.exists(response);
             Should.exist(response.sl);
+            done();
         })
     })
     
-    it('It Should return a translated string, depending upon the query', () =>{
-        Translater.Translate("Hello", "hi") // "hi" is for Hindi
-        .then((response) => {
+    it('It Should return a translated string, depending upon the query', (done) => {
+        Translater.Translate("Hello", "hi").then((response) => {
             assert.exists(response);
             assert.isString(response, 'string');
+            expect(response).to.eql('नमस्ते');
+            done();
         })
     });
     
